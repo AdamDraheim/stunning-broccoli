4f1f0af275df8437310f3a338ba488474971df90
Adam Draheim <acdraheim@gmail.com>
2018-05-16 14:11:35 -0700

    Added a goblin, some blocks, initial physics, and movement

---
+++ Stunning Brocolli/Assets/_script/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("left"))
+        {
+            gameObject.transform.position += new Vector3(-5 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("right"))
+        {
+            gameObject.transform.position += new Vector3(5 * Time.deltaTime, 0, 0);
+        }
+    }
+}
